6a4c8fe3394a74f65906af1712c89cc6f43655e1
jeongyoenwoo <71889210+jeongyoenwoo@users.noreply.github.com>
2024-10-18 17:14:10 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 36.90 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    
+    static Map<String,Integer> map;
+    static int max=0;
+    
+    public String[] solution(String[] orders, int[] course) {
+        List<String> ans = new ArrayList<>();
+        
+        for(int c : course){
+            map = new HashMap<>();
+            max=0;
+            
+            for(String order : orders){
+                char[] strs = order.toCharArray();
+                Arrays.sort(strs);
+                order = new String(strs);
+                
+                dfs(order,"",-1,c,0);
+                
+            }
+            
+            for(String key : map.keySet()){
+                    int value = map.get(key);
+                    if(value>1&&value==max){
+                        ans.add(key);
+                    }
+            }
+        }
+        
+        Collections.sort(ans);
+        String[] answer = ans.toArray(new String[ans.size()]);
+      
+        return answer;
+    }
+    
+    public static void dfs(String order, String key, int index, int end, int depth){
+        if(depth==end){
+            map.put(key,map.getOrDefault(key,0)+1);
+            max = Math.max(max,map.get(key));
+        }
+        
+        for(int i=index+1;i<order.length();i++){
+            dfs(order,key+order.charAt(i),i,end,depth+1);
+        }
+    }
+}
